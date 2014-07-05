6720038051f2b797b1a0a297ac3def036d9f6eb8
IsuruHerath <isuruherath1@gmail.com>
2014-07-05 12:30:07 +0530

    Graph
    Now the pdf object has graphing capabilities

--- PDF.php
+++ PDF.php
@@ -1,17 +1,58 @@
 <?php
-require('fpdf17\fpdf.php');
-class PDF extends FPDF {
+require('lib\diag\diag.php');
+//require('phpgraphlib_v2.31\phpgraphlib.php');
+class PDF extends PDF_Diag {
 			 
 	function Header() {
 		//family, style, size
-		$this->SetTextColor(100,100,100);
-		$this->SetFont('Times','',8);
-		$this->SetY(0.25);
-		//width, height, text, border, ln, align
-		$this->Cell(0, .25, "National Volunteer Secretariat ", '0', 0, "L");
-		$this->Cell(0, .25, $this->PageNo(), '0', 0, "R");
-		//reset Y
-		$this->SetY(1);
+		if($this->PageNo()!=1){
+			$this->SetTextColor(100,100,100);
+			$this->SetFont('Times','',8);
+			$this->SetY(0);
+			//width, height, text, border, ln, align
+			$this->Cell(0, 25, "National Volunteer Secretariat ", '0', 0, "L");
+			$this->Cell(0, 25, $this->PageNo()-1, '0', 0, "R");
+			//reset Y
+			$this->SetY(1);
+		}
+	}
+	
+	function Cover() {
+		$this->SetTextColor(000,100,231);
+		$this->SetFont('Times','',22);
+		$this->SetY(80);
+		//$this->Cell(0, .25, "National Volunteer Secretariat,Sri Lanka", '0', 0, "C");
+		$this->SetX(25.4);
+		$this->Cell(0, 2, "National Volunteer Secretariat,Sri Lanka", '0', 0, "C");
+	}
+	
+	function Table($topics, $data)
+	{
+		$this->SetFont('Arial', 'B', 12);//Set the Font type to Arial,Bold with size 12 Pt
+		$this->SetTextColor(0);//Set the Text Color
+		$this->SetFillColor(000, 188, 225);//Fill the text with RGB Color
+		$this->SetLineWidth(0.2);//Set the Line Width to 1pt
+
+		$this->Cell(30, 7, $topics[0], 'LTR', 0, 'C', true);
+		$this->Cell(30, 7, $topics[1], 'LTR', 0, 'C', true);
+		$this->Cell(30, 7, $topics[2], 'LTR', 0, 'C', true);
+		$this->Cell(50, 7, $topics[3], 'LTR', 1, 'C', true);
+	
+		$this->SetFont('Arial', '');
+		$this->SetFillColor(238);
+		$this->SetLineWidth(0.2);//0.2 pts
+		$fill = false;
+		while($row = mysqli_fetch_array($data)) {
+			$this->Cell(30, 7, $row[$topics[0]], 1, 0, 'C', $fill);
+			$this->Cell(30, 7, $row[$topics[1]], 1, 0, 'C', $fill);
+			$this->Cell(30, 7, $row[$topics[2]], 1, 0, 'C', $fill);
+			$this->Cell(50, 7, $row[$topics[3]], 1, 1, 'R', $fill);
+			$fill = !$fill;
+		}
+	}
+	
+	function Graphs() {
+	
 	}
 	
 	function Footer() {
@@ -20,5 +61,6 @@ class PDF extends FPDF {
 		//y position: rough estimate, width, height, filetype, link: click it!
 			//$this->Image("logo.jpg", (8.5/2)-1.5, 9.8, 3, 1, "JPG", "http://www.phpjabbers.com");
 	}
+
 }
 ?>
--- Report.php
+++ Report.php
@@ -1,29 +1,29 @@
-<!DOCTYPE html>
-<html>
-	<body>
-		<?php
-			
+<?php
 			//require('fpdf17\fpdf.php');
-			require('PDF.php');
-			/*class PDF extends FPDF {
-			 
-				function Header() {
-					$this->SetFont('Times','',12);
-					$this->SetY(0.25);
-					//width, height, text, border, ln, align
-					$this->Cell(0, .25, "National Volunteer Secretariat ", 'T', 0, "L");
-					$this->Cell(0, .25, $this->PageNo(), '0', 0, "R");
-					//reset Y
-					$this->SetY(1);
-				}
-				
-				function Footer() {
-					//This is the footer; it's repeated on each page.
-					//enter filename: phpjabber logo, x position: (page width/2)-half the picture size,
-					//y position: rough estimate, width, height, filetype, link: click it!
-						//$this->Image("logo.jpg", (8.5/2)-1.5, 9.8, 3, 1, "JPG", "http://www.phpjabbers.com");
-				}
-			}*/
+			//require('PDF.php');
+			require('pdf.php');
+			
+			/*
+			
+			$graph = new PHPGraphLib(400,300);
+			$data = array("Alex"=>99, "Mary"=>98, "Joan"=>70, "Ed"=>90);
+			$graph->addData($data);
+			$graph->setTitle("Test Scores");
+			$graph->setTextColor("blue");
+			$graph->createGraph();
+			*/
+			//require('phpgraphlib_v231\phpgraphlib.php');
+			/*
+				include('lib\phpgraphlib_v231\phpgraphlib.php');
+				$graph = new PHPGraphLib(500,350);
+				$data = array(12124, 5535, 43373, 22223, 90432, 23332, 15544, 24523,
+				 32778, 38878, 28787, 33243, 34832, 32302);
+				$graph->addData($data);
+				$graph->setTitle('Widgets Produced');
+				$graph->setGradient('red', 'maroon');
+				//$graph->createGraph();
+			*/
+			
 			// Create connection
 			$con=mysqli_connect("localhost","root","","taxi");
 
@@ -32,47 +32,62 @@
 			  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 			}
 			
-			$result = mysqli_query($con,"SELECT * FROM order_details");
+			$result = mysqli_query($con,"SELECT * FROM daily_income");
+			$topics = array("year","month","date","income");
 			mysqli_close($con);
 			echo "done<br>";
-			while($row = mysqli_fetch_array($result)) {
-				echo $row['jobId']. " " .$row['customer_name'];
-				echo "<br>";
-			}
+			
 			//orientation, units, size
-			$pdf=new PDF("P","in","A4");
- 
-			$pdf->SetMargins(1,1,1);
-			 
-			/*$pdf->AddPage();
-			$pdf->SetFont('Times','',12);
-			 
-			$lipsum1="Lorem ipsum dolor sit amet, nam aliquam dolore est, est in eget.";
-			  
-			$lipsum2="Nibh lectus, pede fusce ullamcorper vel porttitor.";
-			  
-			$lipsum3 ="Duis maecenas et curabitur, felis dolor.";
-			  
-			$pdf->SetFillColor(240, 100, 100);
-			$pdf->SetFont('Times','',12);
-			  
-			//Cell(float w[,float h[,string txt[,mixed border[,
-			//int ln[,string align[,boolean fill[,mixed link]]]]]]])
-			$pdf->Cell(0, .25, "lipsum", 1, 2, "C", 1);
-			  
-			$pdf->SetFont('Times','',12);
-			//MultiCell(float w, float h, string txt [, mixed border [, string align [, boolean fill]]])
-			$pdf->MultiCell(0, 0.5, $lipsum1, 'LR', "L");
-			$pdf->MultiCell(0, 0.25, $lipsum2, 1, "R");
-			$pdf->MultiCell(0, 0.15, $lipsum3, 'B', "J");
-			 
+			$pdf=new PDF("P","mm","A4");
+			//$pdf = new PDF_Diag();
+			$pdf->SetMargins(25.4,25.4,25.4);
 			$pdf->AddPage();
-			$pdf->Write(0.5, $lipsum1.$lipsum2.$lipsum3);*/
-			//ob_clean();
+			
+			$pdf->Cover();
+			$pdf->AddPage();
+			$pdf->SetY(25.4);
+			$pdf->Table($topics, $result);
+			
+			//pie-chart
+			$pdf->SetFont('Arial', 'BIU', 12);
+			$pdf->Cell(0, 5, '1 - Pie chart', 0, 1);
+			$pdf->Ln(8);
+			$data = array('Men' => 1510, 'Women' => 1610, 'Children' => 1400);
+			$pdf->SetFont('Arial', '', 10);
+			$valX = $pdf->GetX();
+			$valY = $pdf->GetY();
+			$pdf->Cell(30, 5, 'Number of men:');
+			$pdf->Cell(15, 5, $data['Men'], 0, 0, 'R');
+			$pdf->Ln();
+			$pdf->Cell(30, 5, 'Number of women:');
+			$pdf->Cell(15, 5, $data['Women'], 0, 0, 'R');
+			$pdf->Ln();
+			$pdf->Cell(30, 5, 'Number of children:');
+			$pdf->Cell(15, 5, $data['Children'], 0, 0, 'R');
+			$pdf->Ln();
+			$pdf->Ln(8);
+
+			$pdf->SetXY(90, $valY);
+			$col1=array(100, 100, 255);
+			$col2=array(255, 100, 100);
+			$col3=array(255, 255, 100);
+			$pdf->PieChart(100, 35, $data, '%l (%p)', array($col1, $col2, $col3));
+			$pdf->SetXY($valX, $valY + 40);
+			
+			//bar chart
+			$pdf->SetFont('Arial', 'BIU', 12);
+			$pdf->Cell(0, 5, '2 - Bar diagram', 0, 1);
+			$pdf->Ln(8);
+			$valX = $pdf->GetX();
+			$valY = $pdf->GetY();
+			$pdf->BarDiagram(200, 30, $data, '%l : %v (%p)', array(255, 175, 100));
+			$pdf->SetXY($valX, $valY + 80);
+			
+			
+			
+			//ob_end_clean();
 			ob_start();
 			//ob_end_clean();
 			$pdf->Output();
 			ob_end_flush();
 		?> 
-	</body>
-</html>
